6f4450ede6f339e9fac166b93ecd51b508dfd9d3
Iannelli Lorenzo <iannelli.lorenzo.studente@itispaleocapa.it>
2024-03-12 08:24:37 +0100

    errore risolto

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
 function createGame() {
     window.open("../html/newgame.html", "_blank");
 }
+
+  
+function getGameID() {
+    // codice postman
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener("readystatechange", function() {
+    if(this.readyState === 4) {
+        console.log(this.responseText);
+    }
+    });
+
+    xhr.open("POST", "https://classe5ID.altervista.org/games/partita/IANNELLI_milionario");
+    xhr.setRequestHeader("Authorization", "Basic NElFOlRpcm9uaQ==");
+
+    xhr.send();
+
+    
+}
